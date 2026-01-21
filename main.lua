ca433cb80bddebfaeff2be2bdab3d2f832195c68
bobsayshilol <bobsayshilol@live.co.uk>
2026-01-21 19:20:11 +0000

    Fix typo

--- main.lua
+++ main.lua
@@ -41,7 +41,7 @@ local s_menus = {
 
 
 -- Fake devices.
-ADD_FAKE_DEVICE = false
+ADD_FAKE_DEVICES = false
 g_fakeDevice = {
     getID = function(self) return -1,-1 end,
     setVibration = function(self, l, r, t) end,
